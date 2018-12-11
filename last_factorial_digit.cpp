04ff307c64d45769d42665cb58f1357f0967b037
Alessandro <masterchieff02@gmail.com>
2018-12-11 22:16:33 +0100

    Refactoring and change title
    It's more speedy with the switch case, and it occupies less memory than a recursive function.
    At the very beginning I overwrite the inserted number with the corresponding last digit, so I have only to print it out int at the end.

---
+++ last_factorial_digit.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+#define MAX 10
+
+using namespace std;
+
+
+int main()
+{
+    int n, fact[MAX];
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> fact[i];
+
+        switch (fact[i])
+        {
+            case 1:
+            {
+                fact[i] = 1;
+                break;
+            }
+
+
+            case 2:
+            {
+                fact[i] = 2;
+                break;
+            }
+
+            case 3:
+            {
+                fact[i] = 3;
+                break;
+            }
+
+
+            case 4:
+            {
+                fact[i] = 4;
+                break;
+            }
+
+            default:
+            {
+                fact[i] = 0;
+                break;
+            }
+        }
+    }
+
+    for (int i = 0; i<n; i++)
+        cout << fact[i] << endl;
+
+    return 0;
+}
