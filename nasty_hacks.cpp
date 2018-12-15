f60af055366846a671e464db7b0d47f79d3d0a43
Alessandro <masterchieff02@gmail.com>
2018-12-15 18:59:22 +0100

    Update nasty_hacks.cpp
    Working

--- nasty_hacks.cpp
+++ nasty_hacks.cpp
@@ -9,30 +9,44 @@ not advertise = 1 R
 not matter = 2 M
 */
 
-int main(){
+int main()
+{
 
     int n,r,e,c;
 
     cin >> n;
 
-    for (int i = 0; i < n; i++) {
-        
+    for (int i = 0; i < n; i++)
+    {
+
         cin >> r;
         cin >> e;
         cin >> c;
 
-        if(r == (e - c)) cout<<"does not matter"<<endl;
-        else {
-
-            if (r < 0 && (e - c) < 0){
-                if(r > (e - c)) cout<< "do not advertise"<<endl;
-                else cout << "advertise"<<endl;
-            } else if ((e - c) > 0){
-                if(r < (e - c)) cout << "advertise"<<endl;
-                else cout<< "do not advertise"<<endl;
+        //IF EQUALS
+        if(r == (e - c))
+            cout<<"does not matter"<<endl;
+        else
+        {
+            //IF BOTH NEGATIVE
+            if (r < 0 && (e - c) < 0)
+            {
+                //IF r LESS NEGATIVE OF (e - c)
+                if(r > (e - c))
+                    cout<< "do not advertise"<<endl;
+                else
+                    cout << "advertise"<<endl;
+            }
+            else //IF BOTH NOT NEGATIVE
+                {
+                    if (r < (e - c) > 0)
+                        cout << "advertise"<<endl;
+                    else
+                        cout<< "do not advertise"<<endl;
+                }
             }
         }
-    }
 
-    return 0; 
+
+    return 0;
 }
