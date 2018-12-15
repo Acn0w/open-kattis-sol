ac60c8eec3a5d4d6b5aeb80c13dfc3440d1323ed
Mikele Milia <45664763+mikelemilia@users.noreply.github.com>
2018-12-15 18:39:42 +0100

    Update nasty_hacks.cpp

--- nasty_hacks.cpp
+++ nasty_hacks.cpp
@@ -4,67 +4,33 @@
 using namespace std;
 #define MAX 100
 
-
 /*
 advertise = 0 E
 not advertise = 1 R
 not matter = 2 M
 */
 
-int main()
-{
-    char s[MAX];
+int main(){
+
     int n,r,e,c;
 
     cin >> n;
 
-    for (int i=0;i<n;i++)
-    {
+    for (int i = 0; i < n; i++) {
+        
         cin >> r;
         cin >> e;
         cin >> c;
 
-        if(r == (e - c))
-            s[n]='m';
-        else
-        {
+        if(r == (e - c)) cout<<"does not matter"<<endl;
+        else {
 
-            if (r<0 && (e-c)<0)
-            {
-                if(r > (e - c))
-                    s[n]='r';
-                else
-                    s[n] ='e';
-            }
-            if ((e-c)>0)
-            {
-                if(r<(e-c))
-                    s[n]='e';
-                else
-                    s[n]='r';
-            }
-        }
-    }
-
-
-    for (int i=0;i<n;i++)
-    {
-        switch(s[n])
-        {
-            case 'e':
-            {
-                cout << "advertise"<<endl;
-                break;
-            }
-            case 'r':
-            {
-                cout<< "do not advertise"<<endl;
-                break;
-            }
-            case 'm':
-            {
-                cout<<"does not matter"<<endl;
-                break;
+            if (r < 0 && (e - c) < 0){
+                if(r > (e - c)) cout<< "do not advertise"<<endl;
+                else cout << "advertise"<<endl;
+            } else if ((e - c) > 0){
+                if(r < (e - c)) cout << "advertise"<<endl;
+                else cout<< "do not advertise"<<endl;
             }
         }
     }
